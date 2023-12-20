33ccf548cc2c119024a6414aa1dd6f4346ec5c7e
gonzalote99 <62606335+gonzalote99@users.noreply.github.com>
2023-12-20 14:13:46 -0300

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# touch-slider
+
+[Edit in Codeflow ⚡️](https://stackblitz.com/~/github.com/gonzalote99/touch-slider)
---
+++ script.js
@@ -0,0 +1,76 @@
+const slider = document.querySelector('.slider-container'),
+slides = Array.from(document.querySelectorAll('.slide'));
+
+
+let isDragging = false,
+startPos = 0,
+currentTranslate = 0,
+prevTranslate = 0,
+animationID = 0,
+currentIndex = 0;
+
+window.oncontextmenu = (event) => {
+  event.preventDefault();
+  event.stopPropagation();
+  return false;
+}
+
+const getPositionX = (event) => 
+  event.type.includes('mouse') ? event.pageX : event.touches[0].clientX;
+
+const setSliderPositon = () => {
+  slider.style.transform = `translateX(${currentTranslate}px)`;
+}
+
+
+const animation = () => {
+  setSliderPositon();
+  if (isDragging) requestAnimationFrame(animation);
+};
+
+const setPositionByIndex = () => {
+  currentTranslate = currentIndex * -window.innerWidth;
+  prevTranslate = currentTranslate;
+  setSliderPositon();
+}
+
+
+const touchStart = (index) => {
+return (event) => {
+currentIndex = index;
+startPos = getPositionX(event);
+isDragging = true;
+animationID = requestAnimationFrame(animation);
+slider.classList.add('grabbing');
+};
+};
+
+const touchEnd = () => {
+  isDragging = false;
+  cancelAnimationFrame(animationID);
+  const movedBy = currentTranslate - prevTranslate;
+  if(movedBy < -100 && currentIndex < slides.length - 1) currentIndex +=1;
+  if(movedBy > 100 && currentIndex > 0) currentIndex -= 1;
+setPositionByIndex();
+slider.classList.remove('grabbing');
+}
+
+const touchMove = (event) => {
+ if(isDragging) {
+   const currentPosition = getPositionX(event);
+    currentTranslate = prevTranslate + currentPosition - startPos;
+ }
+};
+
+
+slides.forEach((slide, index) => {
+const slideImage = slide.querySelector('img');
+slideImage.addEventListener('dragstart', (e) => e.preventDefault());
+slide.addEventListener('touchstart', touchStart(index));
+slide.addEventListener('touchend', touchEnd);
+slide.addEventListener('touchmove', touchMove);
+slide.addEventListener('mousedown', touchStart(index))
+slide.addEventListener('mouseup', touchEnd);
+slide.addEventListener('mouseleave', touchEnd);
+slide.addEventListener('mousemove', touchMove)
+});
